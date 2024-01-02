c77c7fc3293be629f47ba419e5a43b22e61240a2
AYG3 <gilbertayoku3@gmail.com>
2024-01-02 23:31:29 +0100

    s

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,17 @@
-export default function Home() {
-  return (
-    <div>My projects go here</div>
-  )
+import { getProjects } from "@/sanity/sanity.utils"
+
+export default async function Home() {
+  
+  const projects = await getProjects()
+  
+  return <div>
+    
+    {projects.map((project)=>(
+      <div key={project._id}>
+
+      </div>
+    ))}
+    My projects go here
+    
+  </div>
 }
